248d6b86ff7860eba16bc6ce578dec89b3caaad2
Evan Petousis <evan@petousis.net>
2019-06-16 20:15:41 +1000

    Initial Commit

---
+++ EroKit/EroKit.h
@@ -0,0 +1,19 @@
+//
+//  EroKit.h
+//  EroKit
+//
+//  Created by Evangelos Petousis on 16/6/19.
+//  Copyright © 2019 Evangelos Petousis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EroKit.
+FOUNDATION_EXPORT double EroKitVersionNumber;
+
+//! Project version string for EroKit.
+FOUNDATION_EXPORT const unsigned char EroKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EroKit/PublicHeader.h>
+
+
